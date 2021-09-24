928ac45381ae800de2b04a084a5b6f0ccb077ed8
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-09-23 23:55:36 -0700

    commit tree ee98ce73a9e489c12bdcb435c4a04ca8f717d6a7

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries
+
+function summation (acc, element){
+    acc += element
+    return acc
+}
+
+totalBatteries = batteryBatches.reduce(summation , 0)
+
+console.log (totalBatteries)
